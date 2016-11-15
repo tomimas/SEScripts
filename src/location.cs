fc75971bfe495c313bf511c098728e5d958f4096
Tomi Suuronen <tomi@suuronen.eu>
2016-11-15 22:03:16 +0200

    Refactored code to be more easily used.

--- src/location.cs
+++ src/location.cs
@@ -1,23 +1,24 @@
 // by tomimas
 // https://github.com/tomimas/SEScripts
 void Main(String argument) {
-  string STATUS_PANEL = "LCD (Location)";
-  string SOURCE = "Cockpit";
-  IMyTextPanel panel = GridTerminalSystem.GetBlockWithName(STATUS_PANEL) as IMyTextPanel;
-  panel.SetValueColor("FontColor", new Color(255, 255, 255, 255));
-  rewrite(panel, "");
+  string LOCATION_PANEL = "LCD (Location)";
+  IMyTextPanel lPanel = GridTerminalSystem.GetBlockWithName(LOCATION_PANEL) as IMyTextPanel;
 
-  IMyCubeBlock block = GridTerminalSystem.GetBlockWithName(SOURCE) as IMyCubeBlock;
+  positionInfo(lPanel, "Cockpit");
+}
 
-  double x = position(block,0); 
-  double y = position(block,1); 
-  double z = position(block,2);
+void positionInfo(IMyTextPanel panel, String source) {
+  IMyCubeBlock block = GridTerminalSystem.GetBlockWithName(source) as IMyCubeBlock;
+
+  double x = position(block, 0);
+  double y = position(block, 1);
+  double z = position(block, 2);
 
   rewrite(panel, "X: " + x.ToString() + "\nY: " + y.ToString() + "\nZ: " + z.ToString());
 }
 
 double position(IMyCubeBlock block, int axis) {
-  return Math.Round(block.GetPosition().GetDim(axis), 4);
+  return Math.Round(block.GetPosition().GetDim(axis), 2);
 }
 
 void rewrite(IMyTextPanel panel, String text) {
