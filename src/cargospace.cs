a801fa30485299077d58f85a481865b3592eecf1
Tomi Suuronen <tomi@suuronen.eu>
2016-11-15 21:57:39 +0200

    Fixed it to compile, work, and to use latest API.

--- src/cargospace.cs
+++ src/cargospace.cs
@@ -2,29 +2,30 @@
 //https://github.com/tomimas/SEScripts   
 
 void Main(string argument) {
-  string STATUS_PANEL = "LCD (Cargo)";  
-  IMyTextPanel panel = GridTerminalSystem.GetBlockWithName(STATUS_PANEL) as IMyTextPanel;
-  panel.SetValueColor("FontColor", new Color(255, 255, 255, 255));
-  rewrite(panel, "");
+  string CARGO_PANEL = "LCD (Cargo)";  
+  IMyTextPanel cPanel = GridTerminalSystem.GetBlockWithName(CARGO_PANEL) as IMyTextPanel;
 
-  carcgoSpace(panel);
+  cargoSpace(cPanel);
 }
 
-void cargoSpace(iIMyTextPanel panel) {
+void cargoSpace(IMyTextPanel panel) {
   List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
-  GridTerminalSystem.GetBlocksOfType<IMyCockpit>(containers);
-  GridTerminalSystem.GetBlocksOfType<IMyShipDrill>(containers);
-  GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers);
+  List<IMyTerminalBlock> cache = new List<IMyTerminalBlock>();
+  GridTerminalSystem.GetBlocksOfType<IMyCockpit>(cache);
+  containers.AddRange(cache);
+  GridTerminalSystem.GetBlocksOfType<IMyShipDrill>(cache);
+  containers.AddRange(cache);
+  GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(cache);
+  containers.AddRange(cache);
 
   float currentVolume = 0f;
   float maxVolume = 0f;
-  for (int i = 0; i < containers.Count; ++i) {
-    IMyInventoryOwner inventoryOwner = containers[i] as IMyInventoryOwner;
-    IMyInventory inventory = inventoryOwner.GetInventory(0);
+  for (int i = 0; i < containers.Count; i++) {
+    IMyInventory inventory = containers[i].GetInventory(0);
     currentVolume += (float) inventory.CurrentVolume;
     maxVolume += (float) inventory.MaxVolume;
   }
-  rewrite(panel, "Cargo: " + currentVolume + " / " + maxVolume + " (" + Math.Round(currentVolume/maxVolume*100f, 0)  + "%)");
+  rewrite(panel, "Cargo:\n" + (currentVolume * 1000) + " / " + (maxVolume * 1000) + "\n(" + Math.Round(currentVolume/maxVolume*100f, 0)  + "%)");
 }
 
 void rewrite(IMyTextPanel panel, String text) {
